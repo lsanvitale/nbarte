75362c390c96e5c053a3cdeeed331561430b2828
Leonardo.Sanvitale <leonardo.sanvitale@aycron.com>
2012-04-17 19:21:48 -0300

    new rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsinstallerDemo::Application.config.secret_token = '4fc2c0663122625c4e3bc6cbad52eb23e53f9ee0ea985d95c01f5ece83d51752b81fa4033fc8744b37eb5a84f5db29a2d27db8d1cf001af60412754b08c676b5'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_norma_session',
-  :secret      => '35f67b0a54a6fb3156a343a752c50f4f407041b4577ff67f8a62bcdb65039cc36af67a1d8e3a56f851f5204456caf622eeaff9958c80bc9c1be12f9e146864e9'
-}
+RailsinstallerDemo::Application.config.session_store :cookie_store, key: '_railsinstaller_demo_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# RailsinstallerDemo::Application.config.session_store :active_record_store
